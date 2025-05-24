dfe8cdc4dc786d73de79dd1c978afa1b5940e7fc
mbuvic <charlesmbuvipeter@gmail.com>
2025-05-24 08:29:39 +0300

    initial setup routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,3 +9,48 @@ Route::get('/', function () {
 
 Route::get('/b/{username}', [App\Http\Controllers\ProfileController::class, 'showBusinessProfile'])->name('business.profile');
 Route::get('/i/{username}', [App\Http\Controllers\ProfileController::class, 'showIndividualProfile'])->name('individual.profile');
+
+
+Route::get('/clear', function () {
+    // if (!app()->isLocal()) {
+    //     abort(403, 'Unauthorized');
+    // }
+    
+    Artisan::call('config:clear');   // Clear old cached config
+    Artisan::call('cache:clear');    // Clear app cache
+    Artisan::call('route:clear');    // Clear route cache
+    Artisan::call('view:clear');     // Clear compiled Blade views
+    Artisan::call('config:cache');   // Re-cache config from fresh .env
+
+    return 'Cleared!';
+});
+
+//php artisan storage:link
+Route::get('/storage-link', function () {
+    // if (!app()->isLocal()) {
+    //     abort(403, 'Unauthorized');
+    // }
+
+    Artisan::call('storage:link');
+    return 'Storage linked!';
+});
+
+
+Route::get('/migrate-fresh', function () {
+    // if (!app()->isLocal()) {
+    //     abort(403, 'Unauthorized');
+    // }
+
+    Artisan::call('migrate:fresh --seed');
+    return 'Migrated Fresh!';
+});
+
+Route::get('/migrate', function () {
+    // if (!app()->isLocal()) {
+    //     abort(403, 'Unauthorized');
+    // }
+
+    Artisan::call('migrate');
+    return 'Migrated!';
+});
+    
